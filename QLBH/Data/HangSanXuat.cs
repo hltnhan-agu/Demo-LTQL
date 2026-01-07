ee217a2b4fde15219c837299b2670153813df17b
Huỳnh Lý Thanh Nhàn <hltnhan@agu.edu.vn>
2026-01-07 15:51:27 +0700

    #Bài tập 1 # Thêm các class cơ bản

---
+++ QLBH/Data/HangSanXuat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLBH.Data
+{
+    public class HangSanXuat
+    {
+        public int ID { get; set; }
+        public string TenHangSanXuat { get; set; }
+
+        public virtual ObservableCollectionListSource<SanPham> SanPham { get; } = new();
+    }
+}
---
+++ QLBH/Data/LoaiSanPham.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLBH.Data
+{
+    public class LoaiSanPham
+    {
+        public int ID { get; set; }
+        public string TenLoai { get; set; }
+
+        public virtual ObservableCollectionListSource<SanPham> SanPham { get; } = new();
+    }
+}
